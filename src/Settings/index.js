2819e62bc86505a52e849c11ab039547593d43de
iskilled <iskilled@gmail.com>
2018-11-15 14:26:03 +0000

    Add Page wrapper

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
+
 import Welcome from "./WelcomeMessage";
 import ConfirmButton from './ConfirmButton';
+import Page from '../Shared/Page';
 
 export default function() {
   return (
-    <div>
+    <Page name="settings">
       <Welcome />
       <ConfirmButton />
-    </div>
+    </Page>
   );
 }
