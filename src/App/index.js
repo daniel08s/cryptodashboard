cb6295ffea09161f319f8fcd7bb598855266b5b3
iskilled <iskilled@gmail.com>
2018-11-15 11:43:29 +0000

    Add menu bar using CSS Grid

--- src/App/index.js
+++ src/App/index.js
@@ -6,11 +6,13 @@ import styled, { css } from 'styled-components';
 import './App.css';
 import Welcome from './WelcomeMessage';
 import AppLayout from './AppLayout';
+import AppBar from './AppBar';
 
 class App extends Component {
   render() {
     return (
       <AppLayout>
+        <AppBar />
         <Welcome />
       </AppLayout>
     );
