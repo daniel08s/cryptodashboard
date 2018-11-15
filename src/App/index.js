860efa29d80c966bc50b5ddb414d2f7d7a74f279
iskilled <iskilled@gmail.com>
2018-11-15 11:07:28 +0000

    Create a component to display a welcome message

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Welcome from './WelcomeMessage';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World!
+        <Welcome />
       </div>
     );
   }
