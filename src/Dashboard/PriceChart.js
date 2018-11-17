2436a56437c38d6602fd5fd4db4e0c39faf5be85
iskilled <iskilled@gmail.com>
2018-11-17 18:44:21 +0000

    Customize Highcharts theme

--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -4,6 +4,10 @@ import ReactHighcharts from 'react-highcharts';
 import highchartsConfig from './HighchartsConfig';
 import { Tile } from '../Shared/Tile';
 import { AppContext } from '../App/AppProvider';
+import { purpleTheme } from './HighchartsTheme';
+
+// Apply the theme
+ReactHighcharts.Highcharts.setOptions(purpleTheme);
 
 export default () => {
   return (
